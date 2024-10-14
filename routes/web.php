81099ddc16b3648e1c6b1c8a74d3f7284b38dd8c
edwinedgar <e.wahyuardiansyah@gmail.com>
2024-10-15 01:37:47 +0700

    adaa

--- routes/web.php
+++ routes/web.php
@@ -23,8 +23,9 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/tes', [ProfileController::class,'tes']);
 });
 
-Route::get('/tes', [ProfileController::class,'tes']);
+
 
 require __DIR__.'/auth.php';
