d8a861eddf24a154bda26bc5f2290b2e31d6b4d2
edwinedgar <e.wahyuardiansyah@gmail.com>
2024-10-14 23:46:58 +0700

    adaa

--- routes/web.php
+++ routes/web.php
@@ -8,14 +8,16 @@ Route::get('/', function () {
     return view('index');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
 Route::get('/menu-main', [controllermain::class,'menumain']);
 Route::get('/index-components', [controllermain::class,'indexcomponents']);
 Route::get('/index-media', [controllermain::class,'indexmedia']);
 Route::get('/index-pages', [controllermain::class,'indexpages']);
 Route::get('/index-settings', [controllermain::class,'indexsettings']);
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
